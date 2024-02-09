d05e6c7e93b259f6379cf403c42046ba70e6ce96
jjyeon <wodus9193@naver.com>
2024-02-09 16:18:20 +0900

    init commit

---
+++ src/main/java/org/example/gitstudy2/GitStudy2Application.java
@@ -0,0 +1,13 @@
+package org.example.gitstudy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitStudy2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitStudy2Application.class, args);
+    }
+
+}
