f25f6be7cea61dbfa87cb64f76a62f8d2c4a8258
jjyeon <wodus9193@naver.com>
2024-02-09 17:36:22 +0900

    test1

--- src/main/java/org/example/gitstudy2/GitStudy2Application.java
+++ src/main/java/org/example/gitstudy2/GitStudy2Application.java
@@ -9,7 +9,7 @@ public class GitStudy2Application {
 
     public static void main(String[] args) {
         System.out.println();
-        System.out.println(" ");
+        System.out.println("hello");
         System.out.println(" ");
         System.out.println("new line");
         System.out.println("C");
